4b7d63a2ae94b086532f7a9110c7fbb9ac7f76b5
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-03-29 17:45:24 +0500

    initial commmit

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from config import BOT_TOKEN
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
