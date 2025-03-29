fb516e381a11948855b56173563978ad149da1d9
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-03-29 17:55:16 +0500

    update messages.py

--- messages.py
+++ messages.py
@@ -1,4 +1,32 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from validators import url
-from downloader import downloader
+from downloader import insta_downloader
+
+
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    text = update.message.text
+
+    if url(text):
+        data = insta_downloader(text)
+
+        delete_message = await update.message.reply_text(text="⏳")
+
+        if data:
+            await delete_message.delete()
+
+            await update.message.reply_video(
+                video=data,
+                caption="<b> @instagram_downnloaderr_bot </b>",
+                parse_mode = "HTML"
+            )
+
+        else:
+            await delete_message.delete()
+            await update.message.reply_html(
+                text="❌ Video yuklashda xatolik"
+            )
+    else:
+        await update.message.reply_html(
+            text="❌ Manzil xato yuborildi"
+        )
