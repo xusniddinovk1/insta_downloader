6fb49efcc5d6721f2f8db1ffc696662fa080952c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-03-29 18:02:44 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from config import BOT_TOKEN
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from messages import message_handler
+
+# Logging
 import logging
 
 logging.basicConfig(
@@ -12,12 +15,15 @@ logger = logging.getLogger(__name__)
 
 
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
+    await update.message.reply_photo(
+        photo=open("./media/logo.jpg", "rb"),
+        caption=f"Assalomu aleykum {update.effective_user.first_name}\nBotimizga Xush kelibsiz!"
+    )
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start_handler))
+    app.add_handler(MessageHandler(filters.TEXT, message_handler))
 
     app.run_polling()
 
