95b9b4df89fb058a8e07c46d84dbf93a7c9e9251
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-22 19:55:16 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+from config import BOT_TOKEN
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+
+# Logging
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_photo(
+        photo=open("./media/logo.jpg", "rb"),
+        caption=f"Assalomu aleykum {update.effective_user.first_name}\nBotimizga Xush kelibsiz!"
+    )
 
 
-app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start_handler))
+    app.run_polling()
 
-app.add_handler(CommandHandler("hello", hello))
 
-app.run_polling()
+if __name__ == "__main__":
+    main()
