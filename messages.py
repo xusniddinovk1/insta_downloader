9c533d5158a7d5315856f3f900a02d09e976bb7e
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-23 09:54:05 +0500

    initial commit

--- messages.py
+++ messages.py
@@ -1,3 +1,5 @@
+from csv import excel
+
 from telegram import Update
 from telegram.ext import ContextTypes
 from validators import url
@@ -7,16 +9,23 @@ from downloader import insta_downloader
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = update.message.text
 
+    # check message
     if url(message):
-        data = insta_downloader(message)
+        try:
+            data = insta_downloader(message)
 
-        if data:
-            await update.message.reply_video(
-                video=data,
-                caption='📌 <b>@instagraam_savve_bot</b>',
-                parse_mode='HTML'
-            )
-        else:
+            # return video
+            if data:
+                await update.message.reply_video(
+                    video=data,
+                    caption='📌 <b>@instagraam_savve_bot</b>',
+                    parse_mode='HTML'
+                )
+            else:
+                await update.message.reply_html(
+                    text="❌ Video yiklashda xatolik",
+                )
+        except:
             await update.message.reply_html(
                 text="❌ Video yiklashda xatolik",
             )
